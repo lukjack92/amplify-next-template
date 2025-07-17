39026391aafab9128e5243d882d971db1ae3ecf9
lukjack92 <lukjack92@gmail.com>
2025-07-17 12:25:33 +0200

    Changed storage resource!

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,10 +2,10 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'myS3Bucket',
+  //name: 'testowy92',
   access: (allow) => ({
     'protected/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
+      allow.authenticated.to(['read', 'write', 'delete']),
     ]
   })
 });
