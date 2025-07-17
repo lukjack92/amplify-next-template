0cacdad5bbc03c4386351c02a24d3a5c53bd398c
lukjack92 <lukjack92@gmail.com>
2025-07-17 11:15:33 +0200

    Added storage backend

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from '@aws-amplify/ui-react';
+import { StorageBrowser } from "@/components/StorageBrowser";
 
 Amplify.configure(outputs);
 
@@ -33,11 +34,16 @@ export default function App() {
   }
 
   return (
-    <Authenticator hideSignUp={true}>
+    <Authenticator hideSignUp={true} initialState="signIn" variation="default">
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+
+          {/* StorageBrowser Component */}
+          <h2>Your Files</h2>
+          <StorageBrowser />
+
         </main>
       )}
     </Authenticator>
